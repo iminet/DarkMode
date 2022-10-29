ec165f67a93af2c830f09ad51dc2c471f574edd7
Imre Kincses <imre.kincses@oracle.com>
2022-10-29 14:50:00 +0200

    Bugfixes as of reported issue #1

--- DarkMode.ps1
+++ DarkMode.ps1
@@ -2,7 +2,7 @@
 # DARKMODE POWERSHELL SCRIPT
 # The easiest way to set dark/light theme or switch between of those
 # Prerequisites: PowerShell 5.0 or later
-# Version: 1.0
+# Version: 1.1
 # Author: Iminetsoft
 # GitHub: https://github.com/iminet/DarkMode
 # License: CreativeCommons (CC BY 4.0)
@@ -45,53 +45,5 @@ function Toggle
 if ($args.Count -ge 1 -and $args[0] -eq "dark") { SetDark }
 elseif ($args.Count -ge 1 -and $args[0] -eq "light") { SetLight }
 elseif ($args.Count -ge 1 -and $args[0] -eq "status") { Write-Host $(if (IsDark -eq $true) { "Dark theme" } else { "Light theme" } ) }
-=======
-##############################################################################
-# DARKMODE POWERSHELL SCRIPT
-# The easiest way to set dark/light theme or switch between of those
-# Prerequisites: PowerShell 5.0 or later
-# Version: 1.0
-# Author: Iminetsoft
-# GitHub: https://github.com/iminet/DarkMode
-# License: CreativeCommons (CC BY 4.0)
-# Usage: DarkMode.ps1 [status|dark|light|toggle] 
-#        (without the optional switches, the toggle command will be performed)
-##############################################################################
-
-$regkey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize"
-$property = "AppsUseLightTheme"
-$darkvalue = 0
-$lightvalue = 1
-function  SetDark 
-{
-    Set-ItemProperty -Path $regkey -Name $property -Value $darkvalue
-}
-
-function  SetLight 
-{
-    Set-ItemProperty -Path $regkey -Name $property -Value $lightvalue
-}
-
-function IsDark
-{
-    if ($(Get-ItemPropertyValue -Path $regkey -Name $property) -eq $darkvalue)
-    {
-        return $true
-    }
-    else
-    {
-        return $false    
-    }
-}
-
-function Toggle
-{
-    if (IsDark -eq $true) { SetLight }
-    else { SetDark }
-}
-
-if ($args.Count -ge 1 -and $args[0] -eq "dark") { SetDark }
-elseif ($args.Count -ge 1 -and $args[0] -eq "light") { SetLight }
-elseif ($args.Count -ge 1 -and $args[0] -eq "status") { Write-Host $(if (IsDark -eq $true) { "Dark theme" } else { "Light theme" } ) }
->>>>>>> 345d11e52f667be4e06c990f35c19b3143d8e82c
+elseif ($args.Count -ge 1 -and $args[0] -eq "toggle") { Toggle }
 else { Toggle }
